6aef70bb3d14f4d47aff8d9dbb645acf0112fdad
Anni Mari Prees <anpree@ttu.ee>
2020-10-04 09:31:53 +0300

    Data projekti Parties kaust ja klassid

---
+++ Data/Parties/ContactMechanismType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaskShop.Data.Parties
+{
+    public enum ContactMechanismType
+    {
+        Unspecified = 0,
+        PostalAddress = 1,
+        TelecomNumber = 2,
+        ElectronicAddress = 3
+    }
+}
---
+++ Data/Parties/PartyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aids.Enums
+{
+    public enum PartyType
+    {
+        Unspecified = 0,
+        Person = 1,
+        Organization = 2,
+        AutomatedAgent = 3
+    }
+}
