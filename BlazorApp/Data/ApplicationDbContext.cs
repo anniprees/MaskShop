0c34de5710347658155abecc54f982b47a9192b4
carmenteearu@gmail.com <carmenteearu@gmail.com>
2020-10-13 10:47:06 +0300

    ApplicationDbContext sisu lisamine

--- BlazorApp/Data/ApplicationDbContext.cs
+++ BlazorApp/Data/ApplicationDbContext.cs
@@ -1,11 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //    //initializeTables(builder);
+        //}
+
+        //internal void initializeTables(ModelBuilder builder)
+        //{
+        //    MoneyDbContext.InitializeTables(builder);
+        //    QuantityDbContext.InitializeTables(builder);
+        //    PartyDbContext.InitializeTables(builder);
+        //    RulesDbContext.InitializeTables(builder);
+        //    ProductDbContext.InitializeTables(builder);
+        //}
+
     }
 }
