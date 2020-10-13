1b187a35146db094b536005b94f930380f18fff5
Anni Mari Prees <anpree@ttu.ee>
2020-10-13 10:02:22 +0300

    ApplicationDbContext

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -19,13 +19,6 @@ namespace BlazorApp
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                // Configure your authentication provider options here.
-                // For more information, see https://aka.ms/blazor-standalone-auth
-                builder.Configuration.Bind("Local", options.ProviderOptions);
-            });
-
             await builder.Build().RunAsync();
         }
     }
