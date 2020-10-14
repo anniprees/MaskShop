3689fd3212d338f3cd2ea96b30536f3d86aaccef
Allan Päll <alpall@ttu.ee>
2020-10-15 00:11:16 +0300

    Infra/Products DbContext draft added

--- Tests/Data/Products/ColorDataTests.cs
+++ Tests/Data/Products/ColorDataTests.cs
@@ -1,10 +1,10 @@
-﻿using MaskShop.Data.Products;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using MaskShop.Data.Products;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MaskShop.Tests.Data.Products
-{
-    [TestClass]
-    public class ColorDataClassTests : SealedClassTests<ColorData, ProductFeatureData> 
-    {
-    }
-}
+//namespace MaskShop.Tests.Data.Products
+//{
+//    [TestClass]
+//    public class ColorDataClassTests : SealedClassTests<ColorData, ProductFeatureData> 
+//    {
+//    }
+//}
--- Tests/Data/Products/ProductFeatureDataTests.cs
+++ Tests/Data/Products/ProductFeatureDataTests.cs
@@ -1,20 +1,12 @@
-﻿using MaskShop.Aids;
-using MaskShop.Data.Common;
+﻿using MaskShop.Data.Common;
 using MaskShop.Data.Products;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MaskShop.Tests.Data.Products
 {
     [TestClass]
-    public class ProductFeatureDataTests : AbstractClassTests<ProductFeatureData, DefinedEntityData> 
+    public class ProductFeatureDataTests : SealedClassTests<ProductFeatureData, DefinedEntityData> 
     {
-        private class TestClass : ProductFeatureData { }
-
-        [TestInitialize] public override void TestInitialize()
-        {
-            base.TestInitialize();
-            obj = GetRandom.Object<TestClass>();
-        }
     }
 }
 
