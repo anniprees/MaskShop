7054d84e2688ac3527f2096a8a1940dc791b2be7
Anni Mari Prees <anpree@ttu.ee>
2020-10-15 10:09:21 +0300

    Infra Common ja Products testide põhjad

---
+++ Domain/Products/IProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MaskShop.Data.Products;
+using MaskShop.Domain.Common;
+
+namespace MaskShop.Domain.Products
+{
+    public interface IProduct : IUniqueEntity<ProductData>
+    {
+        string ProductCategoryId { get; }
+        ProductCategory ProductCategory { get; }
+    }
+}
