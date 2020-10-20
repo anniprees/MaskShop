2cf804addb913f5d3e8067450b8954e4ed238613
carmenteearu@gmail.com <carmenteearu@gmail.com>
2020-10-20 14:12:59 +0300

    Facade Common ja Products tühjad kaustad

---
+++ Tests/Facade/Common/UniqueEntityTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaskShop.Tests.Facade.Common
+{
+    class UniqueEntityTests
+    {
+    }
+}
---
+++ Tests/Facade/Products/OptionalFeatureViewFactoryTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaskShop.Tests.Facade.Products
+{
+    class OptionalFeatureViewFactoryTests
+    {
+    }
+}
