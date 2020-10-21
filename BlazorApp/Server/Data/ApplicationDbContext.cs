a6c1b0418cba0c3df689613dcd1c57734d0d3600
Anni Mari Prees <anpree@ttu.ee>
2020-10-21 13:11:37 +0300

    Migratsiooni proovimine, ei õnnestunud, rakendus ei võta pilti ette

--- BlazorApp/Server/Data/ApplicationDbContext.cs
+++ BlazorApp/Server/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Extensions;
 using MaskShop.Infra.Products;
 
 namespace BlazorApp.Server.Data
