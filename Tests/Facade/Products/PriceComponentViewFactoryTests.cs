6d9b7dfba15e99e3f3db820fe8f9528562cf5e12
carmenteearu@gmail.com <carmenteearu@gmail.com>
2020-11-23 18:32:09 +0200

    Testide lisamine

---
+++ Tests/Facade/Products/PriceComponentViewFactoryTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MaskShop.Aids;
+using MaskShop.Data.Products;
+using MaskShop.Domain.Products;
+using MaskShop.Facade.Products;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaskShop.Tests.Facade.Products
+{
+    [TestClass]
+    public class PriceComponentViewFactoryTests : BaseTests
+    {
+        [TestInitialize]
+        public void TestInitialize() => type = typeof(PriceComponentViewFactory);
+
+        [TestMethod]
+        public void CreateTest()
+        {
+        }
+
+        [TestMethod]
+        public void CreateObjectTest()
+        {
+            var view = GetRandom.Object<PriceComponentView>();
+            var data = PriceComponentViewFactory.Create(view).Data;
+
+            TestArePropertyValuesEqual(view, data);
+
+        }
+
+        [TestMethod]
+        public void CreateViewTest()
+        {
+            var data = GetRandom.Object<PriceComponentData>();
+            var view = PriceComponentViewFactory.Create(new PriceComponent(data));
+            TestArePropertyValuesEqual(view, data);
+        }
+    }
+}
--- Tests/Infra/Common/BaseRepositoryTests.cs
+++ Tests/Infra/Common/BaseRepositoryTests.cs
@@ -1,109 +1,119 @@
-﻿namespace MaskShop.Tests.Infra.Common
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MaskShop.Aids;
+using MaskShop.Data.Products;
+using MaskShop.Domain.Products;
+using MaskShop.Infra.Common;
+using MaskShop.Infra.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaskShop.Tests.Infra.Common
 {
-    //[TestClass]
-    //public class BaseRepositoryTests : AbstractClassTests<BaseRepository<Product, ProductData>, object>
-    //{
-    //    private class TestClass : BaseRepository<Product, ProductData>
-    //    {
+    [TestClass]
+    public class BaseRepositoryTests : AbstractClassTests<BaseRepository<Product, ProductData>, object>
+    {
+        private class TestClass : BaseRepository<Product, ProductData>
+        {
 
-    //        public TestClass(DbContext c, DbSet<ProductData> s) : base(c, s) { }
-    //        protected override Product ToDomainObject(ProductData d) => new Product(d);
-    //        protected override async Task<ProductData> GetData(string id)
-    //        {
-    //            await Task.CompletedTask;
+            public TestClass(DbContext c, DbSet<ProductData> s) : base(c, s) { }
+            protected override Product ToDomainObject(ProductData d) => new Product(d);
+            protected override async Task<ProductData> GetData(string id)
+            {
+                await Task.CompletedTask;
 
-    //            return await dbSet.FindAsync(id);
-    //        }
-    //        protected override ProductData GetDataById(ProductData d) => dbSet.Find(d.Id);
+                return await dbSet.FindAsync(id);
+            }
+            protected override ProductData GetDataById(ProductData d) => dbSet.Find(d.Id);
 
-    //    }
+        }
 
-    //    [TestInitialize]
-    //    public override void TestInitialize()
-    //    {
-    //        base.TestInitialize();
-    //        var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase("TestDb").Options;
-    //        var c = new ProductDbContext(options);
-    //        obj = new TestClass(c, c.Products);
-    //    }
+        [TestInitialize]
+        //public override void TestInitialize()
+        //{
+        //    base.TestInitialize();
+        //    var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase("TestDb").Options;
+        //    var c = new ProductDbContext(options);
+        //    obj = new TestClass(c, c.Products);
+        //}UseMemoryDatabase ei tööta
 
-    //    [TestMethod]
-    //    public void GetTest()
-    //    {
-    //        var d = GetRandom.Object<ProductData>();
-    //        var o = obj.Get(d.Id).GetAwaiter().GetResult();
-    //        Assert.IsNotNull(o);
-    //        Assert.IsTrue(o.IsUnspecified);
-    //        obj.dbSet.Add(d);
-    //        o = obj.Get(d.Id).GetAwaiter().GetResult();
-    //        Assert.IsNotNull(o);
-    //        Assert.IsInstanceOfType(o, typeof(Product));
-    //        TestArePropertyValuesEqual(d, o.Data);
-    //    }
-    //    [TestMethod]
-    //    public void DeleteTest()
-    //    {
-    //        var d = GetRandom.Object<ProductData>();
-    //        obj.dbSet.Add(d);
-    //        var o = obj.Get(d.Id).GetAwaiter().GetResult();
-    //        Assert.IsNotNull(o);
-    //        obj.Delete(d.Id).GetAwaiter();
-    //        o = obj.Get(d.Id).GetAwaiter().GetResult();
-    //        Assert.IsTrue(o.IsUnspecified);
-    //    }
-    //    [TestMethod]
-    //    public void AddTest()
-    //    {
-    //        var d = GetRandom.Object<ProductData>();
-    //        var o = obj.Get(d.Id).GetAwaiter().GetResult();
-    //        Assert.IsTrue(o.IsUnspecified);
-    //        obj.Add(new Product(d)).GetAwaiter();
-    //        o = obj.Get(d.Id).GetAwaiter().GetResult();
-    //        TestArePropertyValuesEqual(d, o.Data);
-    //    }
-    //    [TestMethod]
-    //    public void UpdateTest()
-    //    {
-    //        var d = GetRandom.Object<ProductData>();
-    //        var d1 = GetRandom.Object<ProductData>();
-    //        obj.Add(new Product(d)).GetAwaiter();
-    //        d1.Id = d.Id;
-    //        obj.Update(new Product(d1)).GetAwaiter();
-    //        var o = obj.Get(d.Id).GetAwaiter().GetResult();
-    //        TestArePropertyValuesEqual(d1, o.Data);
-    //    }
-    //    [TestMethod]
-    //    public void GetListTest()
-    //    {
-    //        for (var i = 0; i < GetRandom.UInt8(10, 30); i++)
-    //        {
-    //            var d = GetRandom.Object<ProductData>();
-    //            obj.Add(new Product(d)).GetAwaiter();
-    //        }
+        [TestMethod]
+        public void GetTest()
+        {
+            var d = GetRandom.Object<ProductData>();
+            var o = obj.Get(d.Id).GetAwaiter().GetResult();
+            Assert.IsNotNull(o);
+            Assert.IsTrue(o.IsUnspecified);
+            obj.dbSet.Add(d);
+            o = obj.Get(d.Id).GetAwaiter().GetResult();
+            Assert.IsNotNull(o);
+            Assert.IsInstanceOfType(o, typeof(Product));
+            TestArePropertyValuesEqual(d, o.Data);
+        }
+        [TestMethod]
+        public void DeleteTest()
+        {
+            var d = GetRandom.Object<ProductData>();
+            obj.dbSet.Add(d);
+            var o = obj.Get(d.Id).GetAwaiter().GetResult();
+            Assert.IsNotNull(o);
+            obj.Delete(d.Id).GetAwaiter();
+            o = obj.Get(d.Id).GetAwaiter().GetResult();
+            Assert.IsTrue(o.IsUnspecified);
+        }
+        [TestMethod]
+        public void AddTest()
+        {
+            var d = GetRandom.Object<ProductData>();
+            var o = obj.Get(d.Id).GetAwaiter().GetResult();
+            Assert.IsTrue(o.IsUnspecified);
+            obj.Add(new Product(d)).GetAwaiter();
+            o = obj.Get(d.Id).GetAwaiter().GetResult();
+            TestArePropertyValuesEqual(d, o.Data);
+        }
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var d = GetRandom.Object<ProductData>();
+            var d1 = GetRandom.Object<ProductData>();
+            obj.Add(new Product(d)).GetAwaiter();
+            d1.Id = d.Id;
+            obj.Update(new Product(d1)).GetAwaiter();
+            var o = obj.Get(d.Id).GetAwaiter().GetResult();
+            TestArePropertyValuesEqual(d1, o.Data);
+        }
+        [TestMethod]
+        public void GetListTest()
+        {
+            for (var i = 0; i < GetRandom.UInt8(10, 30); i++)
+            {
+                var d = GetRandom.Object<ProductData>();
+                obj.Add(new Product(d)).GetAwaiter();
+            }
 
-    //        var l = obj.Get().GetAwaiter().GetResult();
-    //        Assert.AreEqual(obj.dbSet.Count(), l.Count);
-    //    }
+            var l = obj.Get().GetAwaiter().GetResult();
+            Assert.AreEqual(obj.dbSet.Count(), l.Count);
+        }
 
-    //    [TestMethod]
-    //    public void GetByIdTest()
-    //    {
-    //        ProductData data = new ProductData();
-    //        var count = GetRandom.UInt8(10, 30);
-    //        var idx = GetRandom.UInt8(0, count);
+        [TestMethod]
+        public void GetByIdTest()
+        {
+            ProductData data = new ProductData();
+            var count = GetRandom.UInt8(10, 30);
+            var idx = GetRandom.UInt8(0, count);
 
-    //        for (var i = 0; i < count; i++)
-    //        {
-    //            var d = GetRandom.Object<ProductData>();
-    //            if (i == idx) data = d;
-    //            obj.Add(new Product(d)).GetAwaiter();
-    //        }
+            for (var i = 0; i < count; i++)
+            {
+                var d = GetRandom.Object<ProductData>();
+                if (i == idx) data = d;
+                obj.Add(new Product(d)).GetAwaiter();
+            }
 
-    //        var m = obj.GetById(data.Id) as Product;
-    //        Assert.IsNotNull(m);
-    //        TestArePropertyValuesEqual(data, m.Data);
-    //    }
+            var m = obj.GetById(data.Id) as Product;
+            Assert.IsNotNull(m);
+            TestArePropertyValuesEqual(data, m.Data);
+        }
 
-    //}
+    }
 
 }
