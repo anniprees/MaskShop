a720eb743d4ad09033af55b8625ef1533500db38
Anni Mari Prees <anpree@ttu.ee>
2020-12-09 09:09:26 +0200

    Basket ja basket items domain kiht, product price double decimaliks

---
+++ Domain/Orders/BasketItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MaskShop.Data.Orders;
+
+namespace MaskShop.Domain.Orders
+{
+    public sealed class BasketItem : ItemProduct<BasketItemData>
+    {
+        public BasketItem(BasketItemData d) :  base(d) { }
+
+        public string BasketId => Data?.BasketId ?? Unspecified;
+    }
+}
---
+++ Domain/Orders/PartyProducts.cs
@@ -0,0 +1,14 @@
+﻿using MaskShop.Data.Orders;
+using MaskShop.Domain.Common;
+using MaskShop.Domain.Parties;
+
+namespace MaskShop.Domain.Orders
+{
+    public abstract class PartyProducts<TData> : DefinedEntity<TData>
+    where TData : PartyProductsData, new() 
+    {
+        protected PartyProducts(TData d) : base(d) { }
+        public string PartyId => Data?.PartyId ?? Unspecified;
+        public Party Party => new GetFrom<IPartiesRepository, Party>().ById(PartyId);
+    }
+}
