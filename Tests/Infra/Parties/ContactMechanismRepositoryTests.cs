ec796baebc5c571135c189f7024750bf38992521
Lii Saluvere <lii.saluvere@ttu.ee>
2020-12-21 16:31:34 +0200

    Infra Parties teste lisatud

---
+++ Tests/Infra/Parties/ContactMechanismRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MaskShop.Data.Parties;
+using MaskShop.Domain.Parties;
+using MaskShop.Infra;
+using MaskShop.Infra.Common;
+using MaskShop.Infra.Parties;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaskShop.Tests.Infra.Parties
+{
+    [TestClass]
+    public class ContactMechanismsRepositoryTests : ShopDbContextTests<ContactMechanismsRepository, ContactMechanism, ContactMechanismData>
+    {
+        protected override Type GetBaseClass() => typeof(UniqueEntityRepository<ContactMechanism, ContactMechanismData>);
+
+        protected override ContactMechanismsRepository GetObject(ShopDbContext c) =>
+            new ContactMechanismsRepository(c);
+
+        protected override DbSet<ContactMechanismData> GetSet(ShopDbContext c) => c.ContactMechanisms;
+    }
+}
