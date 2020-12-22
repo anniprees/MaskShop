9aa30b422833ad6aabd0704f59d23a19bb0c1c7a
carmenteearu@gmail.com <carmenteearu@gmail.com>
2020-12-22 10:07:18 +0200

    DropdownList hardcoded ei tööta

---
+++ BlazorApp/Client/Shared/Masks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Client.Shared
+{
+    public class Masks
+    {
+        public Masks masks = new Masks();
+        public int MaskId { get; set; }
+        public string Title { get; set; }
+
+    }
+}
