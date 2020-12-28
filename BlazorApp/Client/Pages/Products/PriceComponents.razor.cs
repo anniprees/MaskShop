143fb916037fa662339e512a596c04e75c61f63c
Allan Päll <alpall@ttu.ee>
2020-12-28 12:52:09 +0200

    PriceComponent vaade - lyhem kood

---
+++ BlazorApp/Client/Pages/Products/PriceComponents.razor.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MaskShop.Facade.Products;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp.Client.Pages.Products
+{
+    public partial class PriceComponents
+    {
+        [Inject] private NavigationManager NavigationManager { get; set; }
+        [Inject] private HttpClient HttpClient { get; set; }
+
+        [Parameter] public string Page { get; set; } = "1";
+
+        [Parameter] public string SearchTerm { get; set; } = string.Empty;
+
+        List<PriceComponentView> _priceComponents;
+
+        protected PriceComponentView PriceComponent = new PriceComponentView
+        {
+            Id = "",
+            DiscountPercentage = 0,
+            Comment = "",
+            PartyRoleId = "",
+            ValidFrom = null,
+            ValidTo = null
+        };
+
+        protected string SelectedId { get; set; }
+        protected string ModalTitle { get; set; }
+        protected bool IsAdd { get; set; }
+        protected bool IsView { get; set; }
+        protected bool IsDelete { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+        }
+
+        private void CallLoadData()
+        {
+            Task.Run(async () => { await LoadData(); });
+        }
+
+        protected async Task LoadData()
+        {
+            _priceComponents = await HttpClient.GetJsonAsync<List<PriceComponentView>>("api/pricecomponents");
+            StateHasChanged();
+        }
+
+        protected async Task SearchClick()
+        {
+            if (string.IsNullOrEmpty(SearchTerm))
+            {
+                _priceComponents = await HttpClient.GetJsonAsync<List<PriceComponentView>>(("api/pricecomponents/") + "?name=" + SearchTerm);
+                return;
+            }
+
+            _priceComponents = await HttpClient.GetJsonAsync<List<PriceComponentView>>(("api/pricecomponents/") + "?name=" + SearchTerm);
+            StateHasChanged();
+        }
+
+        protected async Task SearchBoxKeyPress(KeyboardEventArgs ev)
+        {
+            if (ev.Key == "Enter")
+            {
+                await SearchClick();
+            }
+        }
+
+        protected async Task ClearSearch()
+        {
+            SearchTerm = string.Empty;
+            _priceComponents = await HttpClient.GetJsonAsync<List<PriceComponentView>>("api/pricecomponents/");
+            StateHasChanged();
+        }
+        
+        protected void AddPriceComponent()
+        {
+            this.IsAdd = true;
+            this.ModalTitle = "Create price component";
+        }
+
+        protected async Task ViewPriceComponent(string Id)
+        {
+            PriceComponent = await HttpClient.GetJsonAsync<PriceComponentView>("api/pricecomponents/" + Id);
+            SelectedId = Id;
+            this.IsView = true;
+            this.ModalTitle = "View product";
+        }
+
+        protected async Task EditPriceComponent(string Id)
+        {
+            PriceComponent = await HttpClient.GetJsonAsync<PriceComponentView>("api/pricecomponents/" + Id);
+            SelectedId = Id;
+            this.IsAdd = true;
+            this.ModalTitle = "Edit product";
+        }
+
+        protected async Task DeletePriceComponent(string Id)
+        {
+            PriceComponent = await HttpClient.GetJsonAsync<PriceComponentView>("api/pricecomponents/" + Id);
+            SelectedId = Id;
+            this.IsView = true;
+            this.IsDelete = true;
+            this.ModalTitle = "Delete product";
+        }
+
+        protected async Task CreatePriceComponent()
+        {
+            if (SelectedId == null)
+            {
+                await HttpClient.SendJsonAsync(HttpMethod.Post, "api/pricecomponents", PriceComponent);
+            }
+            else
+            {
+                await HttpClient.PutJsonAsync("api/pricecomponents/" + SelectedId, PriceComponent);
+            }
+
+            CloseModal();
+            await OnParametersSetAsync();
+        }
+
+        protected async Task RemovePriceComponent()
+        {
+            await HttpClient.DeleteAsync("api/pricecomponents/" + SelectedId);
+            CloseModal();
+            await OnParametersSetAsync();
+        }
+
+        protected void CloseModal()
+        {
+            PriceComponent = new PriceComponentView();
+            SelectedId = null;
+            this.IsAdd = false;
+            this.IsView = false;
+            this.IsDelete = false;
+            StateHasChanged();
+        }
+    }
+}
---
+++ BlazorApp/Server/Controllers/PriceComponentsController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaskShop.Domain.Products;
+using MaskShop.Facade.Products;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriceComponentsController : ControllerBase
+    {
+        private readonly IPriceComponentsRepository _pr;
+
+        public PriceComponentsController(IPriceComponentsRepository pr)
+        {
+            _pr = pr;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PriceComponentView>>> GetPriceComponents(string name)
+        {
+            var result = await _pr.Get();
+            var aa = new List<PriceComponentView>();
+
+            result.ForEach(x => aa.Add(PriceComponentViewFactory.Create(x)));
+
+            return name == null ? aa : aa.Where(x => x.PartyRoleId.ToLower().Contains(name.ToLower())).ToList();
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PriceComponentView>> GetPriceComponent(string id)
+        {
+            var category = await _pr.Get(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return PriceComponentViewFactory.Create(category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPriceComponent(string id, PriceComponentView category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            await _pr.Update(PriceComponentViewFactory.Create(category));
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PriceComponentView>> PostPriceComponent(PriceComponentView category)
+        {
+            await _pr.Add(PriceComponentViewFactory.Create(category));
+
+            return CreatedAtAction("GetPriceComponent", new { id = category.Id }, category);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletePriceComponent(string id)
+        {
+            await _pr.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
