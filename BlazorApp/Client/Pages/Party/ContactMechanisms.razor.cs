8750c917a265754af05df1d25eecd705500a6d2f
Lii Saluvere <lii.saluvere@ttu.ee>
2020-12-30 12:37:48 +0200

    Contact Mechanisms component

---
+++ BlazorApp/Client/Pages/Party/ContactMechanisms.razor.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MaskShop.Facade.Parties;
+
+namespace BlazorApp.Client.Pages.Party
+{
+    public partial class ContactMechanisms
+    {
+        [Inject] private HttpClient HttpClient { get; set; }
+        [Parameter] public string SearchTerm { get; set; } = string.Empty;
+
+        private int totalPagesQuantity = 1;
+        private int currentPage = 1;
+
+
+        List<ContactMechanismView> _contactMechanisms;
+
+        protected ContactMechanismView ContactMechanism = new ContactMechanismView()
+        {
+            Id ="",
+            Street ="",
+            City ="",
+            State ="",
+            Country ="",
+            ZipCode = "",
+            ElectronicMail ="",
+            ValidFrom = null,
+            ValidTo = null
+        };
+
+        protected string SelectedId { get; set; }
+        protected string ModalTitle { get; set; }
+        protected bool IsAdd { get; set; }
+        protected bool IsView { get; set; }
+        protected bool IsDelete { get; set; }
+        
+        protected override async Task OnParametersSetAsync()
+        {
+            await LoadData();
+        }
+
+        //private async Task SelectedPage(int page)
+        //{
+        //    currentPage = page;
+        //    await LoadPagedData(page);
+        //}
+
+        protected async Task LoadData()
+        {
+            _contactMechanisms = await HttpClient.GetJsonAsync<List<ContactMechanismView>>("api/contactmechanisms");
+        }
+
+        //protected async Task LoadPagedData(int page = 1, int pageSize = 10)
+        //{
+        //    var httpResponse = await HttpClient.GetAsync($"api/contactmechanisms/?page={page}&pageSize={pageSize}");
+        //    if (httpResponse.IsSuccessStatusCode)
+        //    {
+        //        totalPagesQuantity = int.Parse(httpResponse.Headers.GetValues("totalPages").FirstOrDefault());
+        //        var responseString = await httpResponse.Content.ReadAsStringAsync();
+        //        _contactMechanisms = JsonSerializer.Deserialize<List<ContactMechanismView>>(responseString,
+        //            new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        //    }
+        //    else
+        //    {
+        //        //TODO error handling
+        //    }
+        //}
+
+        protected async Task SearchClick()
+        {
+            if (string.IsNullOrEmpty(SearchTerm))
+            {
+                _contactMechanisms = await HttpClient.GetJsonAsync<List<ContactMechanismView>>(("api/contactmechanisms/") + "?name=" + SearchTerm);
+                return;
+            }
+
+            _contactMechanisms = await HttpClient.GetJsonAsync<List<ContactMechanismView>>(("api/contactmechanisms/") + "?name=" + SearchTerm);
+            StateHasChanged();
+        }
+
+        protected async Task SearchBoxKeyPress(KeyboardEventArgs ev)
+        {
+            if (ev.Key == "Enter")
+            {
+                await SearchClick();
+            }
+        }
+
+        protected async Task ClearSearch()
+        {
+            SearchTerm = string.Empty;
+            _contactMechanisms = await HttpClient.GetJsonAsync<List<ContactMechanismView>>("api/contactmechanisms/");
+            StateHasChanged();
+        }
+        
+        protected void AddContactMechanism()
+        {
+            this.IsAdd = true;
+            this.ModalTitle = "Create contact mechanism";
+        }
+
+        protected async Task ViewContactMechanism(string Id)
+        {
+            ContactMechanism = await HttpClient.GetJsonAsync<ContactMechanismView>("api/contactmechanisms/" + Id);
+            SelectedId = Id;
+            this.IsView = true;
+            this.ModalTitle = "View contact mechanism";
+        }
+
+        protected async Task EditContactMechanism(string Id)
+        {
+            ContactMechanism = await HttpClient.GetJsonAsync<ContactMechanismView>("api/contactmechanisms/" + Id);
+            SelectedId = Id;
+            this.IsAdd = true;
+            this.ModalTitle = "Edit contact mechanism";
+        }
+
+        protected async Task DeleteContactMechanism(string Id)
+        {
+            ContactMechanism = await HttpClient.GetJsonAsync<ContactMechanismView>("api/contactmechanisms/" + Id);
+            SelectedId = Id;
+            this.IsView = true;
+            this.IsDelete = true;
+            this.ModalTitle = "Delete contact mechanism";
+        }
+
+        protected async Task CreateContactMechanism()
+        {
+            if (SelectedId == null)
+            {
+                await HttpClient.SendJsonAsync(HttpMethod.Post, "api/contactmechanisms", ContactMechanism);
+            }
+            else
+            {
+                await HttpClient.PutJsonAsync("api/contactmechanisms/" + SelectedId, ContactMechanism);
+            }
+
+            CloseModal();
+            await OnParametersSetAsync();
+        }
+
+        protected async Task RemoveContactMechanism()
+        {
+            await HttpClient.DeleteAsync("api/contactmechanisms/" + SelectedId);
+            CloseModal();
+            await OnParametersSetAsync();
+        }
+
+        protected void CloseModal()
+        {
+            ContactMechanism = new ContactMechanismView()
+            {
+                Id = "",
+                Street = "",
+                City = "",
+                State = "",
+                Country = "",
+                ZipCode = "",
+                ElectronicMail = "",
+                ValidFrom = null,
+                ValidTo = null
+            };
+            SelectedId = null;
+            this.IsAdd = false;
+            this.IsView = false;
+            this.IsDelete = false;
+            StateHasChanged();
+        }
+    }
+}
---
+++ BlazorApp/Server/Controllers/ContactMechanismsController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MaskShop.Domain.Parties;
+using MaskShop.Facade.Parties;
+
+namespace BlazorApp.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactMechanismsController : ControllerBase
+    {
+        private readonly IContactMechanismsRepository _pr;
+
+        public ContactMechanismsController(IContactMechanismsRepository pr)
+        {
+            _pr = pr;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ContactMechanismView>>> GetContactMechanisms(string name)
+        {
+            var result = await _pr.Get();
+            var aa = new List<ContactMechanismView>();
+
+            result.ForEach(x => aa.Add(ContactMechanismViewFactory.Create(x)));
+
+            return name == null ? aa : aa.Where(x => x.Street.ToLower().Contains(name.ToLower())).ToList();
+
+        }
+
+        //[HttpGet("pagination")]
+        //public async Task<ActionResult<List<ContactMechanismView>>> GetPaginated([FromQuery] PaginationDTO pagination)
+        //{
+        //    var result = await _pr.Get();
+        //    var aa = new List<ContactMechanismView>();
+        //    result.ForEach(x => aa.Add(ContactMechanismViewFactory.Create(x)));
+
+        //    var queryable = aa.AsQueryable();
+        //    if (name != null)
+        //    {
+        //        queryable = queryable.Where(x => x.Street.Contains(name));
+        //    }
+
+        //    await HttpContext.InsertPaginationParameterInResponse(queryable, pagination.PageSize);
+        //    return await queryable.Paginate(pagination).ToListAsync();
+        //}
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ContactMechanismView>> GetContactMechanism(string id)
+        {
+            var contactMechanism = await _pr.Get(id);
+
+            if (contactMechanism == null)
+            {
+                return NotFound();
+            }
+
+            return ContactMechanismViewFactory.Create(contactMechanism);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContactMechanism(string id, ContactMechanismView contactMechanism)
+        {
+            if (id != contactMechanism.Id)
+            {
+                return BadRequest();
+            }
+
+            await _pr.Update(ContactMechanismViewFactory.Create(contactMechanism));
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ContactMechanismView>> PostContactMechanism(ContactMechanismView contactMechanism)
+        {
+            await _pr.Add(ContactMechanismViewFactory.Create(contactMechanism));
+
+            return CreatedAtAction("GetContactMechanism", new { id = contactMechanism.Id }, contactMechanism);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteContactMechanism(string id)
+        {
+            await _pr.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
