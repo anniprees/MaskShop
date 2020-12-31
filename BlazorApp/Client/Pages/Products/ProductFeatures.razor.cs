34171fb0706aa1fafea5260f1418eb360885909e
Allan Päll <alpall@ttu.ee>
2020-12-31 08:37:39 +0200

    ProductFeatures must vaade

---
+++ BlazorApp/Client/Pages/Products/ProductFeatures.razor.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MaskShop.Facade.Products;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp.Client.Pages.Products
+{
+    public partial class ProductFeatures
+    {
+        [Inject] private HttpClient HttpClient { get; set; }
+        [Parameter] public string SearchTerm { get; set; } = string.Empty;
+
+        List<ProductFeatureView> _productFeatures;
+
+        protected ProductFeatureView ProductFeature = new ProductFeatureView
+        {
+            Id = "",
+            Name = "",
+            Definition = "",
+            NumericCode = 0,
+            ValidFrom = null,
+            ValidTo = null
+        };
+
+        protected string SelectedId { get; set; }
+        protected string ModalTitle { get; set; }
+        protected bool IsAdd { get; set; }
+        protected bool IsView { get; set; }
+        protected bool IsDelete { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await LoadData();
+        }
+
+        protected async Task LoadData()
+        {
+            _productFeatures = await HttpClient.GetJsonAsync<List<ProductFeatureView>>("api/productfeatures");
+        }
+
+        protected async Task SearchClick()
+        {
+            if (string.IsNullOrEmpty(SearchTerm))
+            {
+                _productFeatures = await HttpClient.GetJsonAsync<List<ProductFeatureView>>(("api/productfeatures/") + "?name=" + SearchTerm);
+                return;
+            }
+
+            _productFeatures = await HttpClient.GetJsonAsync<List<ProductFeatureView>>(("api/productfeatures/") + "?name=" + SearchTerm);
+            StateHasChanged();
+        }
+
+        protected async Task SearchBoxKeyPress(KeyboardEventArgs ev)
+        {
+            if (ev.Key == "Enter")
+            {
+                await SearchClick();
+            }
+        }
+
+        protected async Task ClearSearch()
+        {
+            SearchTerm = string.Empty;
+            _productFeatures = await HttpClient.GetJsonAsync<List<ProductFeatureView>>("api/productfeatures/");
+            StateHasChanged();
+        }
+
+        protected void AddProductFeature()
+        {
+            this.IsAdd = true;
+            this.ModalTitle = "Create price component";
+        }
+
+        protected async Task ViewProductFeature(string Id)
+        {
+            ProductFeature = await HttpClient.GetJsonAsync<ProductFeatureView>("api/productfeatures/" + Id);
+            SelectedId = Id;
+            this.IsView = true;
+            this.ModalTitle = "View product";
+        }
+
+        protected async Task EditProductFeature(string Id)
+        {
+            ProductFeature = await HttpClient.GetJsonAsync<ProductFeatureView>("api/productfeatures/" + Id);
+            SelectedId = Id;
+            this.IsAdd = true;
+            this.ModalTitle = "Edit product";
+        }
+
+        protected async Task DeleteProductFeature(string Id)
+        {
+            ProductFeature = await HttpClient.GetJsonAsync<ProductFeatureView>("api/productfeatures/" + Id);
+            SelectedId = Id;
+            this.IsView = true;
+            this.IsDelete = true;
+            this.ModalTitle = "Delete product";
+        }
+
+        protected async Task CreateProductFeature()
+        {
+            if (SelectedId == null)
+            {
+                await HttpClient.SendJsonAsync(HttpMethod.Post, "api/productfeatures", ProductFeature);
+            }
+            else
+            {
+                await HttpClient.PutJsonAsync("api/productfeatures/" + SelectedId, ProductFeature);
+            }
+
+            CloseModal();
+            await OnParametersSetAsync();
+        }
+
+        protected async Task RemoveProductFeature()
+        {
+            await HttpClient.DeleteAsync("api/productfeatures/" + SelectedId);
+            CloseModal();
+            await OnParametersSetAsync();
+        }
+
+        protected void CloseModal()
+        {
+            ProductFeature = new ProductFeatureView
+            {
+                Id = "",
+                Name = "",
+                Definition = "",
+                NumericCode = 0,
+                ValidFrom = null,
+                ValidTo = null
+            };
+            SelectedId = null;
+            this.IsAdd = false;
+            this.IsView = false;
+            this.IsDelete = false;
+            StateHasChanged();
+        }
+    }
+}
---
+++ BlazorApp/Server/Controllers/ProductFeaturesController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaskShop.Domain.Products;
+using MaskShop.Facade.Products;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductFeaturesController : ControllerBase
+    {
+        private readonly IProductFeaturesRepository _pr;
+
+        public ProductFeaturesController(IProductFeaturesRepository pr)
+        {
+            _pr = pr;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductFeatureView>>> GetProductFeatures(string name)
+        {
+            var result = await _pr.Get();
+            var aa = new List<ProductFeatureView>();
+
+            result.ForEach(x => aa.Add(ProductFeatureViewFactory.Create(x)));
+
+            return name == null ? aa : aa.Where(x => x.Name.ToLower().Contains(name.ToLower())).ToList();
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductFeatureView>> GetProductFeature(string id)
+        {
+            var ProductFeature = await _pr.Get(id);
+
+            if (ProductFeature == null)
+            {
+                return NotFound();
+            }
+
+            return ProductFeatureViewFactory.Create(ProductFeature);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductFeature(string id, ProductFeatureView ProductFeature)
+        {
+            if (id != ProductFeature.Id)
+            {
+                return BadRequest();
+            }
+
+            await _pr.Update(ProductFeatureViewFactory.Create(ProductFeature));
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductFeatureView>> PostProductFeature(ProductFeatureView ProductFeature)
+        {
+            await _pr.Add(ProductFeatureViewFactory.Create(ProductFeature));
+
+            return CreatedAtAction("GetProductFeature", new { id = ProductFeature.Id }, ProductFeature);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProductFeature(string id)
+        {
+            await _pr.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
