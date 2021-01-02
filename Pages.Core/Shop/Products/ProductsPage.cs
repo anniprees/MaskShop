3f9a21681260a57e615fb0e85346dba9932241f3
Allan Päll <alpall@ttu.ee>
2021-01-02 22:21:15 +0200

    Core App Products vaade

---
+++ Pages.Core/Shop/Products/ProductsPage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using MaskShop.Data.Products;
+using MaskShop.Domain.Orders;
+using MaskShop.Domain.Products;
+using MaskShop.Facade.Products;
+using MaskShop.PagesCore.Common;
+using MaskShop.PagesCore.Common.Extensions;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MaskShop.PagesCore.Shop.Products
+{
+    public class ProductsPage : ViewPage<ProductsPage, IProductsRepository, Product, ProductView, ProductData>
+    {
+        public IEnumerable<SelectListItem> Categories { get; }
+
+        public ProductsPage(IProductsRepository r, IProductCategoriesRepository c, IPriceComponentsRepository p) :
+            base(r, "Categories")
+        {
+            Categories = newItemsList<ProductCategory, ProductCategoryData>(c);
+        }
+
+        public string CategoryName(string id) => itemName(Categories, id);
+
+        protected internal override Uri pageUrl() => new Uri("/Shop/Products", UriKind.Relative);
+
+        protected internal override Product toObject(ProductView v) => ProductViewFactory.Create(v);
+        protected internal override ProductView toView(Product o) => ProductViewFactory.Create(o);
+
+        protected override void createTableColumns()
+        {
+            createColumn(x => Item.Id);
+            createColumn(x => Item.Name);
+            createColumn(x => Item.ProductCategoryId);
+            createColumn(x => Item.ProductFeatureApplicabilityId);
+            createColumn(x => Item.PictureUri);
+            createColumn(x => Item.Price);
+            createColumn(x => Item.PriceComponentId);
+            createColumn(x => Item.ValidFrom);
+            createColumn(x => Item.ValidTo);
+        }
+
+        public override IHtmlContent GetValue(IHtmlHelper<ProductsPage> h, int i) => i switch
+        {
+            2 => getRaw(h, CategoryName(Item.ProductCategoryId)),
+            5 => h.DisplayImageFor(Item.PictureUri),
+            _ => base.GetValue(h, i)
+
+        };
+    }
+}
