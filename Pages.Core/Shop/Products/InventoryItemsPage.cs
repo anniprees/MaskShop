876f2b006e60cfaa9fca61e890451c72dc46eab6
Allan Päll <alpall@ttu.ee>
2021-01-02 22:49:36 +0200

    Core App InventoryItems vaade

---
+++ Pages.Core/Shop/Products/InventoryItemsPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using MaskShop.Data.Products;
+using MaskShop.Domain.Products;
+using MaskShop.Facade.Products;
+using MaskShop.PagesCore.Common;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MaskShop.PagesCore.Shop.Products
+{
+    public class InventoryItemsPage : ViewPage<InventoryItemsPage, IInventoryItemsRepository, InventoryItem, InventoryItemView, InventoryItemData>
+    {
+        public IEnumerable<SelectListItem> Products { get; }
+
+        public InventoryItemsPage(IInventoryItemsRepository r, IProductsRepository p) :
+            base(r, "Inventory")
+        {
+            Products = newItemsList<Product, ProductData>(p);
+        }
+
+        public string ProductName(string id) => itemName(Products, id);
+
+        protected internal override Uri pageUrl() => new Uri("/Shop/InventoryItems", UriKind.Relative);
+
+        protected internal override InventoryItem toObject(InventoryItemView v) => InventoryItemViewFactory.Create(v);
+        protected internal override InventoryItemView toView(InventoryItem o) => InventoryItemViewFactory.Create(o);
+
+        protected override void createTableColumns()
+        {
+            createColumn(x => Item.Id);
+            createColumn(x => Item.ProductId);
+            createColumn(x => Item.QuantityOnHand);
+            createColumn(x => Item.ValidFrom);
+            createColumn(x => Item.ValidTo);
+        }
+
+        public override IHtmlContent GetValue(IHtmlHelper<InventoryItemsPage> h, int i) => i switch
+        {
+            1 => getRaw(h, ProductName(Item.ProductId)),
+            _ => base.GetValue(h, i)
+        };
+    }
+}
