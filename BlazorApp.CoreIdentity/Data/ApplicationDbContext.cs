d974245bdd459b41b79236b729d1f6aa27e084ee
Allan Päll <alpall@ttu.ee>
2021-01-02 16:23:06 +0200

    Namespace muutused startup ja dbcontext

--- BlazorApp.CoreIdentity/Data/ApplicationDbContext.cs
+++ BlazorApp.CoreIdentity/Data/ApplicationDbContext.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using MaskShop.Infra;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using MaskShop.Infra;
 
 namespace BlazorApp.CoreIdentity.Data
 {
--- BlazorApp.CoreIdentity/Startup.cs
+++ BlazorApp.CoreIdentity/Startup.cs
@@ -1,4 +1,3 @@
-using BlazorApp.CoreIdentity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlazorApp.CoreIdentity.Data;
 using MaskShop.Domain.Common;
 using MaskShop.Domain.Orders;
 using MaskShop.Domain.Parties;
