1b54836f1c1341b4c4b3e13a749261f52d7f61df
Allan Päll <alpall@ttu.ee>
2021-01-03 16:51:31 +0200

    Products FeatureApplicabilityId maha, products vaate muutused

--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -11,20 +11,13 @@ namespace MaskShop.Domain.Products
         public Product(ProductData d) : base(d) { }
         
         public string ProductCategoryId => Data?.ProductCategoryId ?? Unspecified;
-
         public string PriceComponentId => Data?.PriceComponentId ?? Unspecified;
-
-        public string ProductFeatureApplicabilityId => Data?.ProductFeatureApplicabilityId ?? Unspecified;
-
         public decimal Price => Data?.Price ?? UnspecifiedDecimal;
-
         public string PictureUri => Data?.PictureUri ?? Unspecified;
-
         public byte[] Picture => Data?.Picture ?? new List<byte>().ToArray();
-
-        public ProductCategory ProductCategory => new GetFrom<IProductCategoriesRepository, ProductCategory>().ById(ProductCategoryId);
-        public PriceComponent PriceComponent => new GetFrom<IPriceComponentsRepository, PriceComponent>().ById(PriceComponentId);
-        public ProductFeatureApplicability ProductFeatureApplicability 
-            => new GetFrom<IProductFeatureApplicabilitiesRepository, ProductFeatureApplicability>().ById(ProductFeatureApplicabilityId);
+        public ProductCategory ProductCategory => 
+            new GetFrom<IProductCategoriesRepository, ProductCategory>().ById(ProductCategoryId);
+        public PriceComponent PriceComponent => 
+            new GetFrom<IPriceComponentsRepository, PriceComponent>().ById(PriceComponentId);
     }
 }
