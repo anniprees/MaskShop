5876b1a6eb330db319c3f94ae7c23d7d34d282d1
Allan Päll <alpall@ttu.ee>
2021-01-03 11:06:40 +0200

    Core App Product Feature vaade

---
+++ Pages.Core/Shop/Products/ProductFeaturesPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MaskShop.Data.Products;
+using MaskShop.Domain.Products;
+using MaskShop.Facade.Products;
+using MaskShop.PagesCore.Common;
+
+namespace MaskShop.PagesCore.Shop.Products
+{
+    public class ProductFeaturesPage : ViewPage<ProductFeaturesPage, IProductFeaturesRepository, ProductFeature, ProductFeatureView, ProductFeatureData>
+    {
+
+        public ProductFeaturesPage(IProductFeaturesRepository r) : base(r, "Product Features") { }
+        protected internal override Uri pageUrl() => new Uri("/Shop/ProductFeatures", UriKind.Relative);
+        protected internal override ProductFeature toObject(ProductFeatureView v) => ProductFeatureViewFactory.Create(v);
+        protected internal override ProductFeatureView toView(ProductFeature o) => ProductFeatureViewFactory.Create(o);
+
+        protected override void createTableColumns()
+        {
+            createColumn(x => Item.Id);
+            createColumn(x => Item.Name);
+            createColumn(x => Item.Definition);
+            createColumn(x => Item.NumericCode);
+            createColumn(x => Item.ValidFrom);
+            createColumn(x => Item.ValidTo);
+        }
+    }
+}
