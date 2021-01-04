9dcc14652f9251eb177f4296217174e5914aaf5a
Allan Päll <alpall@ttu.ee>
2021-01-04 14:08:05 +0200

    Data Code delete

--- Tests/Pages.Core/Common/AbstractPageTests.cs
+++ Tests/Pages.Core/Common/AbstractPageTests.cs
@@ -15,22 +15,22 @@ namespace MaskShop.Tests.Pages.Core.Common
     {
 
         internal testRepository db;
-        internal abstract class testClass : ViewsPage<ProductsPage, IProductsRepository, Product, ProductView, ProductData>
-        {
+        //internal abstract class testClass : ViewsPage<ProductsPage, IProductsRepository, Product, ProductView, ProductData>
+        //{
 
-            internal string subTitle { get; set; } = string.Empty;
+        //    internal string subTitle { get; set; } = string.Empty;
 
-            protected internal testClass(IProductsRepository r) : base(r, QuantityPagesNames.SystemsOfUnits) { }
+        //    protected internal testClass(IProductsRepository r) : base(r, QuantityPagesNames.SystemsOfUnits) { }
 
-            protected override Uri pageUrl() => new Uri(QuantityPagesUrls.SystemsOfUnits, UriKind.Relative);
+        //    protected override Uri pageUrl() => new Uri(QuantityPagesUrls.SystemsOfUnits, UriKind.Relative);
 
-            protected override Product toObject(ProductView view) => ProductViewFactory.Create(view);
+        //    protected override Product toObject(ProductView view) => ProductViewFactory.Create(view);
 
-            protected override ProductView toView(Product obj) => ProductViewFactory.Create(obj);
+        //    protected override ProductView toView(Product obj) => ProductViewFactory.Create(obj);
 
-            protected override string pageSubtitle() => subTitle;
+        //    protected override string pageSubtitle() => subTitle;
 
-        }
+        //}
 
         internal class testRepository : UniqueRepository<Product, ProductData>, IProductsRepository { }
 
