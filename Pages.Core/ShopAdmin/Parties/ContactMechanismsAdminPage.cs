0d6937766bcf5e959402892e06788a36bc3418ec
Allan Päll <alpall@ttu.ee>
2021-01-04 18:21:12 +0200

    ContactMechanism vaated

---
+++ Pages.Core/ShopAdmin/Parties/ContactMechanismsAdminPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MaskShop.Domain.Parties;
+using MaskShop.PagesCore.Shop.Parties;
+
+namespace MaskShop.PagesCore.ShopAdmin.Parties
+{
+    public class ContactMechanismsAdminPage : ContactMechanismsPage<ContactMechanismsAdminPage>
+    {
+        public ContactMechanismsAdminPage(IContactMechanismsRepository r) 
+            : base(r, "Contacts") { }
+        
+        protected internal override Uri pageUrl() => new Uri("/Shop/ContactMechanisms", UriKind.Relative);
+        protected override void createTableColumns()
+        {
+            createColumn(x => Item.Id);
+            base.createTableColumns();
+            createColumn(x => Item.ValidFrom);
+            createColumn(x => Item.ValidTo);
+        }
+
+    }
+}
---
+++ Pages.Core/ShopClient/Parties/ContactMechanismsClientPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MaskShop.Domain.Parties;
+using MaskShop.PagesCore.Shop.Parties;
+
+namespace MaskShop.PagesCore.ShopClient.Parties
+{
+    public class ContactMechanismsClientPage : ContactMechanismsPage<ContactMechanismsClientPage>
+    {
+        public ContactMechanismsClientPage(IContactMechanismsRepository r) 
+            : base(r, "My contacts") { }
+
+        protected internal override Uri pageUrl() => new Uri("/Client/ContactMechanisms", UriKind.Relative);
+
+    }
+}
